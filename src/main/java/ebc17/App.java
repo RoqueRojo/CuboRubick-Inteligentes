f4910ffc4df7e1b26384d3fa4fc7782b40bf3808
RoqueRojo <55961143+RoqueRojo@users.noreply.github.com>
2019-10-08 09:00:52 +0200

    Tarea 1

--- src/main/java/ebc17/App.java
+++ src/main/java/ebc17/App.java
@@ -9,7 +9,23 @@ public class App
     public static void main( String[] args ) throws IOException, FileNotFoundException, ParseException
     {
         Cubo cubo = new Cubo (3);
-       cubo.mostrarCara("LEFT");
+        
+        System.out.println("El identificador inical del cubo es de "+cubo.getID());
+        cubo.mostrarCara("FRONT");
+        cubo.moveL(0);
+        cubo.moveB(2);
+        cubo.movel(1);
+        cubo.moveb(1);
+        System.out.println("El identificador despues de realizar una serie de movimientos es de "+cubo.getID());
+        cubo.mostrarCara("FRONT");
+        cubo.moveB(1);
+        cubo.moveL(1);
+        cubo.moveb(2);
+        cubo.movel(0);
+        
+               
+        System.out.println("El identificador despues de realizar los movimientos es de "+cubo.getID());
+        cubo.mostrarCara("FRONT");
         
     }
 }
